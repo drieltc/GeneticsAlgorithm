16a47852d2a8cd382582d7a70586716a438b8348
Adriel Costa <adryeltc2001@gmail.com>
2025-03-26 18:08:45 -0300

    <feat:created items const>

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,12 +1,27 @@
-const GENES = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890, .-;:_!\"#%&/()=?@${[]}"
-const TARGET = "Hello, World!"
-
-function generateRandomString(lenght){
-    let randomString = ""
-    for(let i =0; i < lenght; i++){
-        let randomNumber = Math.floor(Math.random() * GENES.length)
-        randomString += GENES[randomNumber]
-    }
-    return randomString
-}
-console.log(generateRandomString(13))
+const itens = [
+    { nome: "Laptop", peso: 2.0, tamanho: 3000, apreco: 9 },
+    { nome: "Livro", peso: 0.5, tamanho: 500, apreco: 7 },
+    { nome: "Fones de ouvido", peso: 0.2, tamanho: 100, apreco: 8 },
+    { nome: "Câmera", peso: 1.0, tamanho: 800, apreco: 9 },
+    { nome: "Carregador de celular", peso: 0.3, tamanho: 200, apreco: 8 },
+    { nome: "Power bank", peso: 0.4, tamanho: 300, apreco: 7 },
+    { nome: "Kit de higiene pessoal", peso: 0.8, tamanho: 600, apreco: 6 },
+    { nome: "Remédios", peso: 0.2, tamanho: 100, apreco: 10 },
+    { nome: "Documentos importantes", peso: 0.1, tamanho: 50, apreco: 10 },
+    { nome: "Lanches", peso: 0.5, tamanho: 400, apreco: 5 },
+    { nome: "Garrafa de água reutilizável", peso: 0.6, tamanho: 700, apreco: 6 },
+    { nome: "Blusa extra", peso: 0.7, tamanho: 900, apreco: 7 },
+    { nome: "Meias extras", peso: 0.2, tamanho: 200, apreco: 5 },
+    { nome: "Cachecol", peso: 0.3, tamanho: 300, apreco: 6 },
+    { nome: "Óculos de sol", peso: 0.1, tamanho: 100, apreco: 7 },
+    { nome: "Chapéu", peso: 0.2, tamanho: 200, apreco: 6 },
+    { nome: "Travesseiro de viagem", peso: 0.4, tamanho: 500, apreco: 8 },
+    { nome: "Máscara de dormir", peso: 0.1, tamanho: 50, apreco: 7 },
+    { nome: "Livro de colorir e lápis de cor", peso: 0.6, tamanho: 600, apreco: 5 },
+    { nome: "Pequeno kit de costura", peso: 0.2, tamanho: 100, apreco: 4 },
+  ];
+  
+  // Exemplo de como acessar os dados de um item
+  console.log(itens[0].nome); // Saída: Laptop
+  console.log(itens[0].peso); // Saída: 2
+  
